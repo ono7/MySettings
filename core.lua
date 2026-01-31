e0bcfc03bfdf37d8e43b6c88e102c56314a4536c
Lima, Jose <20385388+ono7@users.noreply.github.com>
2026-01-31 15:22:29 -0600

    core.lua: fix

--- core.lua
+++ core.lua
@@ -22,9 +22,9 @@ end
 -- Sets the CVar, then retrieves it to verify the engine accepted it
 local function SetAndVerifyCVar(cvar, value)
   SetCVar(cvar, value)
-  Log("SET" .. cvar, actualValue)
+  Log("SET -> " .. cvar, value)
   local actualValue = GetCVar(cvar)
-  Log("ACTUAL" .. cvar, actualValue)
+  Log("ACTUAL -> " .. cvar, actualValue)
 end
 
 -- 3. INITIALIZATION & PVP OPTIMIZATIONS
